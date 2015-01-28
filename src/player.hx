8f822c481e7fe7edef16f52fe9593b93d53d1308
zowiehi <zhaugaard@gmail.com>
2015-01-28 11:44:38 -0700

    Added a player class
    added a new player class in player.hx

---
+++ src/player.hx
@@ -0,0 +1,23 @@
+
+import starling.core.Starling;
+
+
+class Player extends Sprite {
+	
+
+		public var health:	int;
+		public var choice: 	int = 0;
+
+
+		public function new () {
+			super();
+		}
+
+		
+
+
+
+
+
+
+}
